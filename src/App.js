948ff894ff2577d4fda442e0ee82c10b8bc7c1ec
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-03-14 17:54:16 +0530

    UI work Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Banner */}
+      <div className="container-fluid p-5 bg-primary text-white text-center">
+        <h2>Github Profile Search</h2>
+        <p></p>
+      </div>
+      {/*Input or Button Components */}
+      <div className="container mt-5 col-sm-6 text-center">
+        <label className="control-label">Github Username</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter Username"
+          id="username"
+        />
+        <button type="button" className="btn btn-primary col-sm-4 m-2">
+          Find
+        </button>
+      </div>
+      {/* Listing Components */}
+      <div className="container">hii</div>
+    </>
   );
 }
 
